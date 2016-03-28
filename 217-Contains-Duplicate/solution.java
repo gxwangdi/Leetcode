1a43a96b248c656d86ba608b4c9cd8748a26b2ef
Di Wang <gxwangdi@gmail.com>
2016-03-28 00:05:57 -0700

    [217. Contains Duplicate][Accepted]committed by Di

---
+++ 217-Contains-Duplicate/solution.java
@@ -0,0 +1,19 @@
+
+
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return false;
+        }
+        
+        Arrays.sort(nums); 
+        for (int i=0; i<nums.length-1; i++) {
+            if (nums[i] == nums[i+1]) {
+                return true;
+            }
+        }
+        return false;
+    }// end of containsDuplicate   
+}
+
+
