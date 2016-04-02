10af28ad6eee67d9ace8d1b3ae8e625a2ae46757
Di Wang <gxwangdi@gmail.com>
2016-04-02 12:29:04 -0700

    [258. Add Digits][Accepted]committed by Di

---
+++ 258-Add-Digits/solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int addDigits(int num) {
+        if (num/10 < 1) {
+            return num;
+        }
+        
+        num = Math.abs(num);
+        return 1+((num-1)%9);
+    }// end of addDigits    
+}
