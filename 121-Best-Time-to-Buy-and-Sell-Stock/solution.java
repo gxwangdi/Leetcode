801831f57f054a4b6d965520a663b3274fe233fb
Di Wang <gxwangdi@gmail.com>
2016-05-23 23:22:19 -0700

    [121. Best Time to Buy and Sell Stock][Accepted]committed by Di

---
+++ 121-Best-Time-to-Buy-and-Sell-Stock/solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length <2) {
+            return 0;
+        }
+        
+        int profit = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i=0; i<prices.length; i++) {
+            profit = Math.max(profit, prices[i]-min);
+            min = Math.min(min, prices[i]);
+        }
+        return profit;
+    }// end of maxProfit
+}
+
+
