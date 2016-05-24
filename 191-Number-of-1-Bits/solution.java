42cdce8c73425811b93600f2e85c53e38f1af6f1
Di Wang <gxwangdi@gmail.com>
2016-05-23 23:28:02 -0700

    [191. Number of 1 Bits][Accepted]committed by Di

---
+++ 191-Number-of-1-Bits/solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        // n = Math.abs(n);
+        String s = Integer.toBinaryString(n);
+        s = s.replaceAll("0", "");
+        return s.length();
+    }
+}
