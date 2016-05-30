232f682d1b3cf1eb2d91ab03e67ccbca8ee5c0eb
Di Wang <gxwangdi@gmail.com>
2016-05-30 10:28:38 -0700

    [1. Two Sum][Accepted]committed by Di

---
+++ 1-Two-Sum/solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length <2) {
+            return null;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] res = new int[2];
+        for (int i=0; i<nums.length; i++) {
+            int needed = target - nums[i];  
+            if (map.containsKey(nums[i])) {
+                res[0] = map.get(nums[i]);
+                res[1] = i;
+                return res;
+            }
+            map.put(needed, i);
+        }
+        return res;
+    }// end of twoSum
+}
