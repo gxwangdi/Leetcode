75f1c8af98975467b581a807f62aded8b5934f26
Di Wang <gxwangdi@gmail.com>
2016-06-11 13:44:02 -0700

    [125. Valid Palindrome][Accepted]committed by Di

---
+++ 125-Valid-Palindrome/solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+    
+    public boolean isPalindrome(String s) {
+        if (s==null || s.length() <2) {
+            return true;
+        }
+        
+        s = s.toLowerCase();
+        s = s.trim();
+        s = s.replaceAll("[^a-z0-9]", "");
+        
+        int left = 0;
+        int right = s.length()-1;
+        while (left <right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
+
