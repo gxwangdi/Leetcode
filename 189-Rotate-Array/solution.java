d885c9956355e0f005045ad5413a2a69be8a52bd
Di Wang <gxwangdi@gmail.com>
2016-06-11 13:22:48 -0700

    [189. Rotate Array][Accepted]committed by Di

--- 189-Rotate-Array/solution.java
+++ 189-Rotate-Array/solution.java
@@ -1,10 +1,40 @@
+
+
 public class Solution {
+    
     public void rotate(int[] nums, int k) {
         if (nums == null || k<=0 || nums.length<=0 || k== nums.length) {
             return;
         }
         if (k>nums.length) {
-            k = k - nums.length;
+            k = k % nums.length;
+        }
+        int a = nums.length - k;
+        reverse(nums, 0, a-1);
+        reverse(nums, a, nums.length-1);
+        reverse(nums, 0, nums.length-1);
+    }
+    
+    private void reverse(int[] nums, int left, int right) {
+        if (nums == null || nums.length == 1)
+            return;
+        
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+    }
+    
+    /*
+    public void rotate(int[] nums, int k) {
+        if (nums == null || k<=0 || nums.length<=0 || k== nums.length) {
+            return;
+        }
+        if (k>nums.length) {
+            k = k % nums.length;
         }
         int[] rotate = new int[k];
         for (int i=0, j=nums.length-k; j<nums.length; i++, j++) {
@@ -19,4 +49,7 @@ public class Solution {
             nums[i] = rotate[i];
         }
     }//end of rotate 
+    */
 }
+
+
