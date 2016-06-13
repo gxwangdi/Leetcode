0819bc632481fb36941168ac7b27ebb2a3f4736a
Di Wang <gxwangdi@gmail.com>
2016-06-12 20:12:05 -0700

    [112. Path Sum][Accepted]committed by Di

---
+++ 112-Path-Sum/solution.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) 
+            return false;
+        
+        return dfs(root, sum, 0);
+    }
+    
+    private boolean dfs(TreeNode node, int sum, int cur) {
+        cur += node.val;
+        if (node.left == null && node.right == null) 
+            return cur == sum;
+        
+        boolean result = false;
+        if (node.left!=null)
+            result = result || dfs(node.left, sum, cur);
+        if (node.right!=null)
+            result = result || dfs(node.right, sum, cur);
+        return result;
+    }
+}
