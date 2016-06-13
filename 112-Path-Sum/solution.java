89be603d8dd13a41ece7aca4ba4c7f9a7232321d
Di Wang <gxwangdi@gmail.com>
2016-06-12 20:14:47 -0700

    [112. Path Sum][Accepted]committed by Di

--- 112-Path-Sum/solution.java
+++ 112-Path-Sum/solution.java
@@ -8,6 +8,18 @@
  * }
  */
 public class Solution {
+    
+    public boolean hasPathSum(TreeNode root, int sum) {
+	    if (root == null)
+		    return false;
+	    if (root.val == sum && (root.left == null && root.right == null))
+		    return true;
+ 
+	    return hasPathSum(root.left, sum - root.val)
+			|| hasPathSum(root.right, sum - root.val);
+    }
+    
+    /*
     public boolean hasPathSum(TreeNode root, int sum) {
         if (root == null) 
             return false;
@@ -26,5 +38,6 @@ public class Solution {
         if (node.right!=null)
             result = result || dfs(node.right, sum, cur);
         return result;
-    }
+    }*/
+    
 }
