c543783aade0f0215e41d68139568ef773425fa9
Di Wang <gxwangdi@gmail.com>
2016-06-13 10:59:37 -0700

    [28. Implement strStr()][Accepted]committed by Di

--- 28-Implement-strStr()/solution.java
+++ 28-Implement-strStr()/solution.java
@@ -1,7 +1,22 @@
 public class Solution {
+    
+    public int strStr(String haystack, String needle) {
+        if (haystack == null || needle == null || haystack.length() < needle.length())
+            return -1;
+            
+        Set<String> set = new HashSet<>();
+        set.add(needle);
+        for (int i=0; i<=haystack.length()-needle.length(); i++) {
+            if (set.contains(haystack.substring(i, i+needle.length())))
+                return i;
+        }
+        return -1;
+    }
+    
+    /* // Java API 
     public int strStr(String haystack, String needle) {
         if (haystack == null) 
             return -1;
         return haystack.indexOf(needle);
-    }
+    }*/
 }
