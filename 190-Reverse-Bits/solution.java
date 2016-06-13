90621993279df753c92f19a50556b4d3c36fc7f8
Di Wang <gxwangdi@gmail.com>
2016-06-12 22:40:34 -0700

    [190. Reverse Bits][Accepted]committed by Di

--- 190-Reverse-Bits/solution.java
+++ 190-Reverse-Bits/solution.java
@@ -1,5 +1,37 @@
 public class Solution {
     
+    // For functions getting called very frequently, cache some intermediate result.
+    // you need treat n as an unsigned value
+    private final Map<Byte, Integer> cache = new HashMap<Byte, Integer>();
+public int reverseBits(int n) {
+    byte[] bytes = new byte[4];
+    for (int i = 0; i < 4; i++) // convert int into 4 bytes
+        bytes[i] = (byte)((n >>> 8*i) & 0xFF);
+    int result = 0;
+    for (int i = 0; i < 4; i++) {
+        result += reverseByte(bytes[i]); // reverse per byte
+        if (i < 3)
+            result <<= 8;
+    }
+    return result;
+}
+
+private int reverseByte(byte b) {
+    Integer value = cache.get(b); // first look up from cache
+    if (value != null)
+        return value;
+    value = 0;
+    // reverse by bit
+    for (int i = 0; i < 8; i++) {
+        value += ((b >>> i) & 1);
+        if (i < 7)
+            value <<= 1;
+    }
+    cache.put(b, value);
+    return value;
+}
+    
+    /*
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
         int res = 0;
@@ -8,6 +40,7 @@ public class Solution {
         }
         return res;
     }
+    */
     
     /*
     // Java Integer.reverse() source code. 
