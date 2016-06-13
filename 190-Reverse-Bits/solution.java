568f1d4ccab677c272294d0a5a7584d3725fc017
Di Wang <gxwangdi@gmail.com>
2016-06-12 22:32:26 -0700

    [190. Reverse Bits][Accepted]committed by Di

--- 190-Reverse-Bits/solution.java
+++ 190-Reverse-Bits/solution.java
@@ -1,5 +1,15 @@
 public class Solution {
     
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int res = 0;
+        for(int i = 0; i < 32; i++, n >>= 1){
+            res = res << 1 | (n & 1);
+        }
+        return res;
+    }
+    
+    /*
     // Java Integer.reverse() source code. 
     // you need treat n as an unsigned value
     public int reverseBits(int i) {
@@ -8,5 +18,5 @@ public class Solution {
         i = (i & 0x0f0f0f0f) << 4 | (i >>> 4) & 0x0f0f0f0f;
         i = (i << 24) | ((i & 0xff00) << 8) | ((i >>> 8) & 0xff00) | (i >>> 24);
         return i;
-    }
+    }*/
 }
