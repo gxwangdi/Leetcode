2f3510d05c8cb596725046489c138867429a85c4
Di Wang <gxwangdi@gmail.com>
2016-06-12 22:07:02 -0700

    [278. First Bad Version][Accepted]committed by Di

--- 278-First-Bad-Version/solution.java
+++ 278-First-Bad-Version/solution.java
@@ -6,7 +6,7 @@ public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int start = 1, end = n;
         while (start + 1 < end) {
-            int mid = start + (end - start) / 2;
+            int mid = start + (end - start) / 2; // mid = (end + start)/2; exceeds time limit! 
             if (isBadVersion(mid)) {
                 end = mid;
             } else {
