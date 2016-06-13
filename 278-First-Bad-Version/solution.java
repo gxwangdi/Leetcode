ce93ff783585fe9812133bd206b2126a74d25c4f
Di Wang <gxwangdi@gmail.com>
2016-06-12 22:06:00 -0700

    [278. First Bad Version][Accepted]committed by Di

---
+++ 278-First-Bad-Version/solution.java
@@ -0,0 +1,41 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    
+    public int firstBadVersion(int n) {
+        int start = 1, end = n;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (isBadVersion(mid)) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+            
+        if (isBadVersion(start)) {
+            return start;
+        }
+        return end;
+    }
+    
+    /*   // Time limit exceeds. Be very conservertive to use isBadVersion(int), treat it as a time consuming op.
+    public int firstBadVersion(int n) {
+        if (n<1 || isBadVersion(1))
+            return 1;
+        int start = 1;
+        int end = n;
+        while (start+1<end) {
+            int mid = (start+end)/2;
+            if (isBadVersion(mid)) {
+                if (!isBadVersion(mid-1))
+                    return mid;
+                end = mid-1;
+            } else {
+                start = mid+1;
+            }
+        }
+        return start;
+    }*/
+}
