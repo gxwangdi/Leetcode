3649eb05f6c4fd0552d85f6b9a8b7706b3fe2dd9
Di Wang <gxwangdi@users.noreply.github.com>
2016-06-20 11:33:15 -0700

    [204. Count Primes][Accepted]committed by Di

--- 204-Count-Primes/solution.java
+++ 204-Count-Primes/solution.java
@@ -1,6 +1,38 @@
 
 
 public class Solution {
+    
+    /*
+        Serach online and found a similar approach, but more concise implementation. 
+        Based on https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes. 
+    */
+    public int countPrimes(int n) {
+        if (n<3) {
+            return 0;
+        }
+        
+        boolean[] primes = new boolean[n];
+        Arrays.fill(primes, true);
+        primes[0] = false;
+        primes[1] = false;
+        for (int i=2; i*i<n; i++) {
+            if (primes[i]) {
+                for (int j=i+i; j<n; j+=i) {
+                    primes[j] = false;
+                }
+            }
+        }
+        int count = 0;
+        for (boolean b:primes) {
+            if (b) {
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    // ==================== Below is my own implementation ====================
+    /*
     public int countPrimes(int n) {
         List<Integer> list = new ArrayList<>();
         if (n<3) {
@@ -30,6 +62,7 @@ public class Solution {
         }
         return false;
     }// end of isPrime.
+    */
 }
 
 
