a30765a41df0ca1af9ddd29fb5ba9f095563bfb6
Di Wang <gxwangdi@users.noreply.github.com>
2016-06-22 17:32:50 -0700

    [35. Search Insert Position][Accepted]committed by Di

--- 35-Search-Insert-Position/solution.java
+++ 35-Search-Insert-Position/solution.java
@@ -1,4 +1,26 @@
 public class Solution {
+    
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        return searchInsert(nums, target, 0, nums.length-1);
+    }
+    
+    private int searchInsert(int[] nums, int target, int start, int end) {
+        int mid = start + (end - start)/2;
+        
+        if (target == nums[mid]) {
+            return mid;
+        } else if (target < nums[mid]) {
+            return start < mid? searchInsert(nums, target, start, mid-1):start;
+        } else {
+            return end > mid? searchInsert(nums, target, mid+1, end):(end+1);
+        }
+    }
+    
+    /*
     public int searchInsert(int[] nums, int target) {
         if (nums == null) {
             return Integer.MIN_VALUE;
@@ -36,4 +58,8 @@ public class Solution {
         }
         return Integer.MAX_VALUE;
     }
+    */
 }
+
+
+
