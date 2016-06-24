bca5bcaf8513032cdd989fac9467aa97eb2fc2ad
Di Wang <gxwangdi@users.noreply.github.com>
2016-06-24 13:11:04 -0700

    [81. Search in Rotated Sorted Array II][Accepted]committed by Di

---
+++ 81-Search-in-Rotated-Sorted-Array-II/solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public boolean search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        for (int i=0; i<nums.length; i++) {
+            if (nums[i] == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+
+
