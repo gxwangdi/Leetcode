f6f8970e56c6a8207595cf71429d81f386f0578f
Di Wang <gxwangdi@users.noreply.github.com>
2016-06-25 10:24:20 -0700

    [145. Binary Tree Postorder Traversal][Accepted]committed by Di

--- 145-Binary-Tree-Postorder-Traversal/solution.java
+++ 145-Binary-Tree-Postorder-Traversal/solution.java
@@ -8,7 +8,56 @@
  * }
  */
 public class Solution {
-    // recursive manner  
+    
+    
+    private static class Pair {
+        public TreeNode node;
+        public boolean visited;
+        public Pair(TreeNode tn, boolean v) {
+            node = tn;
+            visited = v;
+        }
+    }
+    
+    
+    // Iterative manner.        
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TempNode> stack = new Stack<>();
+        TreeNode node = root;
+        TempNode temp;
+        while (node!=null || !stack.empty()) {
+            while (node != null) {
+                temp = new TempNode(node, true);
+                stack.push(temp);
+                node = node.left;
+            }
+            if (!stack.empty()) {
+                temp = stack.pop();
+                if (temp.isFirst) {
+                    temp.isFirst = false;
+                    stack.push(temp);
+                    node = temp.node.right;
+                } else {
+                    res.add(temp.node.val);
+                    node = null;
+                }
+            }
+        }
+        return res;
+    }
+    
+    private static class TempNode {
+        public TreeNode node;
+        public boolean isFirst;
+        public TempNode (TreeNode tn, boolean first) {
+            node = tn;
+            isFirst = first;
+        }
+    }
+    
+    
+    /* // recursive manner  
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         postorder(root, res);
@@ -22,7 +71,8 @@ public class Solution {
         postorder(node.left, list);
         postorder(node.right, list);
         list.add(node.val);
-    }
+    }*/  
+    
 }
 
 
