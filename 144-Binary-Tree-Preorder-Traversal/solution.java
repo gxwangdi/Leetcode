1e556903846f258882eb995ae034a9001b073910
Di Wang <gxwangdi@users.noreply.github.com>
2016-06-25 00:35:53 -0700

    [144. Binary Tree Preorder Traversal][Accepted]committed by Di

---
+++ 144-Binary-Tree-Preorder-Traversal/solution.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    
+    // recursive manner   
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        preorder(root, res);
+        return res;
+    }
+    
+    private void preorder(TreeNode node, List<Integer> list) {
+        if (node == null) {
+            return;
+        }
+        list.add(node.val);
+        preorder(node.left, list);
+        preorder(node.right, list);
+    }
+}
