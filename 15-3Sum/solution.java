011f32719aa4115b650f1ec5ac7e3af0a9b336fa
Di Wang <gxwangdi@users.noreply.github.com>
2016-06-27 13:31:49 -0700

    [15. 3Sum][Accepted]committed by Di

--- 15-3Sum/solution.java
+++ 15-3Sum/solution.java
@@ -1,4 +1,48 @@
 public class Solution {
+    
+    /* // For case that requires index, then cannot sort.  Map approach. 
+    // Time Limit exceeds.   
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length<3) {
+            return res;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i=0; i<nums.length; i++) {
+            int balance = 0-nums[i];
+            if (!map.containsKey(balance)) {
+                map.put(balance, i);
+            }
+            // map.put(balance, i);
+        }
+        
+        Set<List<Integer>> duplicateSet = new HashSet<>();
+        for (int i=0; i<nums.length-1; i++) {
+            for (int j=i+1; j<nums.length; j++) {
+                int sum = nums[i] + nums[j];
+                if (!map.containsKey(sum)) {
+                    continue;
+                }
+                int index = map.get(sum);
+                if (index == i || index == j) {
+                    continue;
+                }
+                List<Integer> list = new ArrayList<>(3);
+                list.add(nums[i]);
+                list.add(nums[j]);
+                list.add(nums[index]);
+                Collections.sort(list);
+                if (!duplicateSet.contains(list)) {
+                    res.add(list);
+                    duplicateSet.add(list);
+                }
+                // res.add(list);
+            }
+        }
+        return res;
+    }*/
+    
+      // Two pointers approach
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         if (nums == null || nums.length<3) {
@@ -36,4 +80,9 @@ public class Solution {
         }
         return res;
     }// 
+    
 }
+
+
+
+
