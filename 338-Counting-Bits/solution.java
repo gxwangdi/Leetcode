8db049d337170d18086e273b6019739596852c55
Di Wang <gxwangdi@users.noreply.github.com>
2016-06-30 12:46:20 -0700

    [338. Counting Bits][Accepted]committed by Di

--- 338-Counting-Bits/solution.java
+++ 338-Counting-Bits/solution.java
@@ -1,6 +1,22 @@
 public class Solution {
     
-    // res[i] = res[i&(i-1)] + 1, starts from res[1]. 
+    public int[] countBits(int num) {
+        if (num < 0) {
+            return null;
+        } 
+        int[] res = new int[num+1];
+        res[0] = 0;
+        for (int i=1; i<res.length; i++) {
+            if (i%2 == 0) {
+                res[i] = res[i/2];
+            } else {
+                res[i] = res[i/2]+1;
+            }
+        }
+        return res;
+    }
+    
+    /* // res[i] = res[i&(i-1)] + 1, starts from res[1]. 
     public int[] countBits(int num) {
         if (num <0) {
             return null;
@@ -11,5 +27,11 @@ public class Solution {
             res[i] = res[i&(i-1)] +1;
         }
         return res;
-    }
+    }*/
 }
+
+
+
+
+
+
