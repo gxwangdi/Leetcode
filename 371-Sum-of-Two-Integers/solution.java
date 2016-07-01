2229003acc3812e169428e7e3ec841d16248d95e
Di Wang <gxwangdi@users.noreply.github.com>
2016-07-01 11:27:28 -0700

    [371. Sum of Two Integers][Accepted]committed by Di

--- 371-Sum-of-Two-Integers/solution.java
+++ 371-Sum-of-Two-Integers/solution.java
@@ -1,29 +1,6 @@
-public class Solution {
-    
-    /* // Recursive approach
-    public int getSum(int a, int b) {
-        if (b == 0) {
-            return a;
-        }
-        int sum = a ^ b;
-        int carry = (a & b)<<1;
-        return getSum(sum, carry);
-    }*/
-    
-    public int getSum(int a, int b) {
-        if (a == 0) {
-            return b;
-        }
-        if (b == 0) {
-            return a;
-        }
-        while (b!=0) {
-            int carry = a&b;
-            a = a ^ b;
-            b = carry<<1;
-        }
-        return a;
+class Solution {
+public:
+    int getSum(int a, int b) {
+        
     }
-}
-
-
+};
