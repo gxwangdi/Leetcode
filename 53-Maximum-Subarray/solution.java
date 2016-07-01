e9843778fdaf7393818097f1f3c7a728612b3457
Di Wang <gxwangdi@users.noreply.github.com>
2016-06-30 18:13:50 -0700

    [53. Maximum Subarray][Accepted]committed by Di

--- 53-Maximum-Subarray/solution.java
+++ 53-Maximum-Subarray/solution.java
@@ -1,5 +1,6 @@
 public class Solution {
-    // Greedy    
+    
+    /* // Greedy    
     public int maxSubArray(int[] nums) {
         if (nums == null || nums.length == 0) {
             return 0;
@@ -11,6 +12,24 @@ public class Solution {
             sum += nums[i];
             max = Math.max(max, sum);
             sum = Math.max(sum, 0);
+        }
+        return max;
+    }
+    */
+    
+    // Prefix Sum 
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        int minSum = 0;
+        for (int i=0; i<nums.length; i++) {
+            sum += nums[i];
+            max = Math.max(max, sum-minSum);
+            minSum = Math.min(minSum, sum);
         }
         return max;
     }
