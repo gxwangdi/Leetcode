0c818f72031f1ece22fe975e04031a0b8adc8542
Di Wang <gxwangdi@users.noreply.github.com>
2016-06-30 17:38:04 -0700

    [137. Single Number II][Accepted]committed by Di

--- 137-Single-Number-II/solution.java
+++ 137-Single-Number-II/solution.java
@@ -7,9 +7,9 @@ public class Solution {
         int[] bits = new int[32];
         for (int i=0; i<32; i++) {
             for (int j : nums) {
-                bits[i] += (j>>i)&1;
-                bits[i] = bits[i]%3;
+                bits[i] += (j>>i)&1;  // check if ith bit is 1 for j
             }
+            bits[i] = bits[i]%3; // every 3 clear.
             bits[i] = bits[i]<<i;
             result = result|bits[i];
         }
