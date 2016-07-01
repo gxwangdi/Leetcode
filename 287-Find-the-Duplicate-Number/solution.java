dc1cfd7b40e28f9696185758b0f7851a1d3f5c7c
Di Wang <gxwangdi@users.noreply.github.com>
2016-06-30 23:51:19 -0700

    [287. Find the Duplicate Number][Accepted]committed by Di

--- 287-Find-the-Duplicate-Number/solution.java
+++ 287-Find-the-Duplicate-Number/solution.java
@@ -17,7 +17,7 @@ public class Solution {
             }
         }
         return right;
-    }
+    }*/
     
     private int numBelow(int[] nums, int target) {
         int res = 0;
@@ -27,25 +27,19 @@ public class Solution {
             }
         }
         return res;
-    }*/
+    }
     
     public int findDuplicate(int[] nums) {
-    int n=nums.length-1;
-    int left=0, right=n; 
-    while(left<right){
-        int mid=left+(right-left)/2;
-        int count=numBelow(nums, mid);
-        if(count>mid) right=mid;
-        else left=mid+1;
+        int n=nums.length-1;
+        int left=0, right=n; 
+        while(left<right){
+            int mid=left+(right-left)/2;
+            int count=numBelow(nums, mid);
+            if(count>mid) right=mid;
+            else left=mid+1;
+        }
+        return left;
     }
-    return left;
-}
-public int numBelow(int[] nums, int target){
-    int result=0;
-    for(int i=0; i<nums.length; i++)
-        if(nums[i]<=target) result++;
-    return result;
-  }
     
     /* // O(n) solution from online. 
     public int findDuplicate(int[] nums) {
