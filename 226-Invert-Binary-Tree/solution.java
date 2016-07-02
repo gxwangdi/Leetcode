029879ab23c79c7b5f58a1751f46756f80c94de4
Di Wang <gxwangdi@users.noreply.github.com>
2016-07-02 10:15:23 -0700

    [226. Invert Binary Tree][Accepted]committed by Di

--- 226-Invert-Binary-Tree/solution.java
+++ 226-Invert-Binary-Tree/solution.java
@@ -9,41 +9,20 @@
  */
  
 public class Solution {
+    
+    // Recursive approach
     public TreeNode invertTree(TreeNode root) {
         if (root == null) {
             return root;
         }
-        TreeNode newRoot = new TreeNode(root.val);
-        newRoot.left = invertTree(root.right);
-        newRoot.right = invertTree(root.left);
-        return newRoot;
-        // TreeNode newRoot = null;
-        // if (root!= null) {
-        //     newRoot = invertLeftRight(root);
-        // }
-        
-        // if (newRoot != null) {
-        //     newRoot.left = invertTree(newRoot.left);
-        //     newRoot.right = invertTree(newRoot.right);
-        // }
-        // return newRoot;
+        // TreeNode newRoot = new TreeNode(root.val);
+        // newRoot.left = invertTree(root.right);
+        // newRoot.right = invertTree(root.left);
+        TreeNode temp = root.right;
+        root.right = invertTree(root.left);
+        root.left = invertTree(temp);
+        return root;
     }
-    
-    // private TreeNode invertLeftRight(TreeNode node) {
-    //     TreeNode left = null;
-    //     TreeNode right = null;
-        
-    //     if (node.left!=null) {
-    //         left = node.left;
-    //     }
-    //     if (node.right!=null) {
-    //         right = node.right;
-    //     }
-    //     TreeNode newNode = new TreeNode(node.val);
-    //     newNode.left = right;
-    //     newNode.right = left;
-    //     return newNode;
-    // }// end of invertLeftRight      
 }// end of Solution class   
 
 
