7f688417bc1cc256bf8dd421ddf3626b055c804b
Di Wang <gxwangdi@users.noreply.github.com>
2016-07-14 12:27:17 -0700

    [199. Binary Tree Right Side View][Accepted]committed by Di

---
+++ 199-Binary-Tree-Right-Side-View/solution.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> currLevel = new LinkedList<>();
+        currLevel.offer(root);
+        Queue<TreeNode> nextLevel = new LinkedList<>();
+        Queue<TreeNode> temp;
+        while (!currLevel.isEmpty()) {
+            boolean isFirst = true;
+            while (!currLevel.isEmpty()) {
+                TreeNode node = currLevel.poll();
+                if (isFirst) {
+                    res.add(node.val);
+                    isFirst = false;
+                }
+                if (node.right != null) {
+                    nextLevel.offer(node.right);
+                }
+                if (node.left != null) {
+                    nextLevel.offer(node.left);
+                }
+            }
+            temp = nextLevel;
+            nextLevel = currLevel;
+            currLevel = temp;
+        }
+        return res;
+    }
+}
+
+
+
+
