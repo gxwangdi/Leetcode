1a138b37e9a9b3da19277de209928a38a7628829
Di Wang <gxwangdi@users.noreply.github.com>
2016-07-14 16:01:56 -0700

    [7. Reverse Integer][Accepted]committed by Di

--- 7-Reverse-Integer/solution.java
+++ 7-Reverse-Integer/solution.java
@@ -1,15 +1,4 @@
 public class Solution {
-    /*
-    public int reverse(int x) {
-        int rev = 0;
-        while(x != 0){
-            rev = rev*10 + x%10;
-            x = x/10;
-        }
- 
-        return rev;
-    }*/
-    
     
     public int reverse(int x) {
         
