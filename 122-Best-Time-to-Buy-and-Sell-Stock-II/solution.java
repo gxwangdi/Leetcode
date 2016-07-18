5c2477c15915be11b1c89dbdade0787ac5fe84cf
Di Wang <gxwangdi@users.noreply.github.com>
2016-07-17 22:21:56 -0700

    [122. Best Time to Buy and Sell Stock II][Accepted]committed by Di

---
+++ 122-Best-Time-to-Buy-and-Sell-Stock-II/solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        if (prices == null || prices.length == 0) {
+            return profit;
+        }
+        int min = prices[0];
+        int cur = 1;
+        while (cur < prices.length) {
+            if (prices[cur] < prices[cur-1]) {
+                profit = profit + prices[cur-1] - min;
+                min = prices[cur];
+            }
+            cur++;
+        }
+        profit = profit + prices[cur-1] - min;
+        return profit;
+    }
+}
+
+
+
