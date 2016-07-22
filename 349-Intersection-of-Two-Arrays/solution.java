b1bdd3ef44f2f8c7a1ec3671dd2854c23256669e
Di Wang <gxwangdi@users.noreply.github.com>
2016-07-21 20:22:51 -0700

    [349. Intersection of Two Arrays][Accepted]committed by Di

--- 349-Intersection-of-Two-Arrays/solution.java
+++ 349-Intersection-of-Two-Arrays/solution.java
@@ -1,4 +1,38 @@
 public class Solution {
+    
+    public int[] intersection(int[] nums1, int[] nums2) {
+        if (nums1==null || nums2==null || nums1.length ==0 || nums2.length ==0) {
+            return new int[0];
+        }
+        
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i =0;
+        int j =0;
+        int last = Integer.MIN_VALUE;
+        List<Integer> list = new ArrayList<>();
+        while (i<nums1.length && j<nums2.length) {
+            if (nums1[i] == nums2[j] && nums1[i]!=last) {
+                list.add(nums1[i]);
+                last = nums1[i];
+                i++;
+                j++;
+                continue;
+            }
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        int[] res = new int[list.size()];
+        for (int cur =0; cur<res.length; cur++) {
+            res[cur] = list.get(cur);
+        }
+        return res;
+    }
+    
+    /*
     public int[] intersection(int[] nums1, int[] nums2) {
         if (nums1==null || nums2==null || nums1.length ==0 || nums2.length ==0)
             return new int[0];
@@ -32,6 +66,7 @@ public class Solution {
         
         return reslist;
     }// end 
+    */
 }
 
 
