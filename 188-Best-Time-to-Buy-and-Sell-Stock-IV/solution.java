73876f41b431d7fe2f085d9cca9666811f858969
Di Wang <gxwangdi@users.noreply.github.com>
2016-07-23 00:06:57 -0700

    [188. Best Time to Buy and Sell Stock IV][Accepted]committed by Di

--- 188-Best-Time-to-Buy-and-Sell-Stock-IV/solution.java
+++ 188-Best-Time-to-Buy-and-Sell-Stock-IV/solution.java
@@ -45,10 +45,13 @@ public class Solution {
     private int solveMaxProfit(int[] prices) {
         int max = 0;
         for (int i=1; i<prices.length; i++) {
-            if (prices[i] - prices[i-1] > 0) {
+            if (prices[i] > prices[i-1]) {
                 max += prices[i] - prices[i-1];
             }
         }
         return max;
     }// end of solveMaxProfit     
 }
+
+
+
