910551e843baaddcda5be98db256838f4e1da2fa
Di Wang <gxwangdi@users.noreply.github.com>
2016-07-24 20:55:50 -0700

    [3. Longest Substring Without Repeating Characters][Accepted]committed by Di

--- 3-Longest-Substring-Without-Repeating-Characters/solution.java
+++ 3-Longest-Substring-Without-Repeating-Characters/solution.java
@@ -26,6 +26,62 @@ public class Solution {
         res = Math.max(res, len - start);
         return res;
     }
+    
+    
+    /*
+    // My implementation is over complecated. 
+    public int lengthOfLongestSubstring(String s) {
+        // write your code here
+        int max = 0;
+        if (s == null || s.length() ==0) {
+            return max;
+        }
+        
+        int slow = 0;
+        int fast = 1;
+        Map<Character, Integer> map = new HashMap<>();
+        map.put(s.charAt(slow), 1);
+        max = 1;
+        
+        while (fast < s.length()) {
+            // move fast forward until there is a repeated char
+            while (fast < s.length() && !isRepeated(map)) {
+                char c = s.charAt(fast);
+                if (map.containsKey(c)) {
+                    map.put(c, map.get(c)+1);
+                } else {
+                    map.put(c, 1);
+                }
+                fast++;
+            }
+            max = Math.max(max, fast-slow-1);
+            if (fast == s.length()) {
+                break;
+            }
+            // move slow forward until it is back to non-repeated.
+            char last = s.charAt(fast-1);
+            char cur = s.charAt(slow);
+            while (slow < fast && cur!=last) {
+                map.put(cur, map.get(cur)-1);
+                slow++;
+                cur = s.charAt(slow);
+            }
+            map.put(cur, map.get(cur)-1);
+            slow++;
+        }
+        return max;
+    }
+    
+    
+    private boolean isRepeated(Map<Character, Integer> map) {
+        for (Character c : map.keySet()) {
+            if (map.get(c) > 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+    */
 }
 
 
