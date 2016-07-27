0a90912255ea2f86280a6ba00783df0663eb06b0
Di Wang <gxwangdi@users.noreply.github.com>
2016-07-26 22:15:04 -0700

    [56. Merge Intervals][Accepted]committed by Di

---
+++ 56-Merge-Intervals/solution.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class Solution {
+    public List<Interval> merge(List<Interval> intervals) {
+        if (intervals == null || intervals.size()<2) {
+            return intervals;
+        }
+        
+        Collections.sort(intervals, new Comparator<Interval>(){
+            public int compare(Interval i1, Interval i2) {
+                if (i1.start != i2.start) {
+                    return i1.start - i2.start;
+                }
+                return i1.end - i2.end;
+            }
+        });
+        
+        List<Interval> res = new ArrayList<>();
+        Interval pivot = intervals.get(0);
+        for (int i=1; i<intervals.size(); i++) {
+            Interval cur = intervals.get(i);
+            if (pivot.end < cur.start) {
+                res.add(pivot);
+                pivot = cur;
+            } else {
+                pivot.start = Math.min(pivot.start, cur.start);
+                pivot.end = Math.max(pivot.end, cur.end);
+            }
+        }
+        res.add(pivot);
+        return res;
+    }
+}
+
+
+
+
