0e18a3c6f640aa939282b7e8f0b6929fcd2ac5ee
Di Wang <gxwangdi@users.noreply.github.com>
2016-08-01 18:25:56 -0700

    [254. Factor Combinations][Accepted]committed by Di

---
+++ 254-Factor-Combinations/solution.java
@@ -0,0 +1,34 @@
+public class Solution {
+    public List<List<Integer>> getFactors(int n) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (n <4) {
+            return res;
+        }
+        helper(res, n, -1, new ArrayList<Integer>());
+        return res;
+    }
+    
+    private void helper(List<List<Integer>> res, int n, int a, List<Integer> prog) {
+        if (a != -1) {
+            List<Integer> list = new ArrayList<>(prog);
+            list.add(n);
+            res.add(list);
+        }
+        
+        int upper = (int)Math.sqrt(n);
+        int cur = Math.max(2, a);
+        while (cur <= upper) {
+            if (n%cur == 0) {
+                List<Integer> list = new ArrayList<>(prog);
+                list.add(cur);
+                helper(res, n/cur, cur, list);
+            }
+            cur++;
+        }
+    }// end of helper    
+}
+
+
+
+
+
