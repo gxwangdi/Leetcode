a3df8ca7a8188316f2024b888eb0db0389e9202e
Di Wang <gxwangdi@users.noreply.github.com>
2016-08-04 19:14:56 -0700

    [261. Graph Valid Tree][Accepted]committed by Di

--- 261-Graph-Valid-Tree/solution.java
+++ 261-Graph-Valid-Tree/solution.java
@@ -1,5 +1,47 @@
 public class Solution {
     
+    // BFS
+    public boolean validTree(int n, int[][] edges) {
+        if (n < 1 || edges == null) {
+            return false;
+        }
+        if (edges.length != n-1) {
+            return false;
+        }
+        int[] visited = new int[n];
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i=0; i<n; i++) {
+            adj.add(new ArrayList<Integer>());
+        }
+        for (int[] edge : edges) {
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+        }
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(0);
+        visited[0] =1;
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            for (int succ : adj.get(cur)) {
+                if (visited[succ] == 1) {
+                    return false; // has cycle
+                }
+                if (visited[succ] == 0) {
+                    q.offer(succ);
+                    visited[succ] = 1;
+                }
+            }
+            visited[cur] = 2; // visite completed.
+        }
+        for (int v:visited) {
+            if (v == 0) {
+                return false;
+            } 
+        }
+        return true;
+    }
+    
+    /*
     // DFS   
     public boolean validTree(int n, int[][] edges) {
         if (n < 1 || edges == null) {
@@ -46,6 +88,9 @@ public class Solution {
         visited[cur] = 2;
         return false;
     }// end of hasCycle   
+    */
+    
+    
 }
 
 
