475721f8e1002f183a2daa3cabadc2b49b89c0dc
Di Wang <gxwangdi@users.noreply.github.com>
2016-08-04 20:16:36 -0700

    [274. H-Index][Accepted]committed by Di

--- 274-H-Index/solution.java
+++ 274-H-Index/solution.java
@@ -1,4 +1,33 @@
 public class Solution {
+    
+    // O(n)
+    public int hIndex(int[] citations) {
+        if (citations == null || citations.length == 0) {
+            return 0;
+        }
+        
+        int len = citations.length;
+        int[] count = new int[len+1];
+        for (int i=0; i<len; i++) {
+            if (citations[i] > len) {
+                count[len]++;
+            } else {
+                count[citations[i]]++;
+            }
+        }
+        int t = 0;
+        int res = 0;
+        for (int i=len; i>=0; i--) {
+            t += count[i];
+            if (t >= i) {
+                return i;
+            }
+        }
+        return 0;
+    }
+    
+    /*
+    // O(nlogn) solution
     public int hIndex(int[] citations) {
         if (citations == null || citations.length == 0) {
             return 0;
@@ -23,6 +52,7 @@ public class Solution {
         }
         return res;
     }// end of hindex     
+    */
 }
 
 
