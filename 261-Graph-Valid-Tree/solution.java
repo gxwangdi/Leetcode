205356f0b41e1e8c51f744237cd5499e2805115c
Di Wang <gxwangdi@users.noreply.github.com>
2016-08-04 19:30:18 -0700

    [261. Graph Valid Tree][Accepted]committed by Di

--- 261-Graph-Valid-Tree/solution.java
+++ 261-Graph-Valid-Tree/solution.java
@@ -1,5 +1,59 @@
 public class Solution {
     
+    // Union Find solution
+    public boolean validTree(int n, int[][] edges) {
+        UnionFind uf = new UnionFind(n);
+        for (int[] edge:edges) {
+            int x = edge[0];
+            int y = edge[1];
+            if (!uf.union(x, y)) {
+                return false;
+            }
+        }
+        return uf.count == 1;
+    }
+    
+    private static class UnionFind {
+        int[] parent;
+        int[] rank;
+        int count;
+        
+        UnionFind(int n) {
+            parent = new int[n];
+            rank = new int[n];
+            count = n;
+            for (int i=0; i<n; i++) {
+                parent[i] = i;
+            } 
+        }
+        
+        int find(int x) {
+            if (x!=parent[x]) {
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }// end of find   
+        
+        boolean union(int x, int y) {
+            int X = find(x);
+            int Y = find(y);
+            if (X == Y) {
+                return false;
+            }
+            if (rank[X] > rank[Y]) {
+                parent[Y] = X;
+            } else if (rank[Y] > rank[X]) {
+                parent[X] = Y;
+            } else {
+                rank[X]++;
+                parent[Y] = X;
+            }
+            count--;
+            return true;
+        }
+    }
+    
+    /*
     // BFS
     public boolean validTree(int n, int[][] edges) {
         if (n < 1 || edges == null) {
@@ -40,6 +94,7 @@ public class Solution {
         }
         return true;
     }
+    */
     
     /*
     // DFS   
