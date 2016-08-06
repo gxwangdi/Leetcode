e5124edcbd3d39ac89a1d6ab168485f082ae8894
Di Wang <gxwangdi@users.noreply.github.com>
2016-08-05 19:17:39 -0700

    [362. Design Hit Counter][Accepted]committed by Di

--- 362-Design-Hit-Counter/solution.java
+++ 362-Design-Hit-Counter/solution.java
@@ -4,12 +4,20 @@ public class HitCounter {
     
     /** Initialize your data structure here. */
     public HitCounter() {
+        // map = new ConcurrentHashMap<>();
         map = new HashMap<>();
     }
     
     /** Record a hit.
         @param timestamp - The current timestamp (in seconds granularity). */
     public void hit(int timestamp) {
+        // synchronized (map) {
+        //     if (map.containsKey(timestamp)) {
+        //     map.put(timestamp, map.get(timestamp)+1);
+        // } else {
+        //     map.put(timestamp, 1);
+        // }
+        // }
         if (map.containsKey(timestamp)) {
             map.put(timestamp, map.get(timestamp)+1);
         } else {
@@ -21,12 +29,24 @@ public class HitCounter {
         @param timestamp - The current timestamp (in seconds granularity). */
     public int getHits(int timestamp) {
         int sum = 0;
-        int cur = timestamp-300+1;
-        while (cur <= timestamp) {
-            if (map.containsKey(cur)) {
-                sum += map.get(cur);
+        // synchronized (map) {
+        //     for (Integer key : map.keySet()) {
+        //     if (timestamp - key >= 300) {
+        //         map.remove(key);
+        //     } else {
+        //         sum += map.get(key);
+        //     }
+        // }
+        // }
+        for (Integer key : map.keySet()) {
+            // if (timestamp - key >= 300) {
+            //     map.remove(key);
+            // } else {
+            //     sum += map.get(key);
+            // }
+            if (timestamp - key < 300) {
+                sum += map.get(key);
             }
-            cur++;
         }
         return sum;
     }// 
