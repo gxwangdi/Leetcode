f0ac2728d7f276f961aed9db970d2eab69fe6c0f
Di Wang <gxwangdi@users.noreply.github.com>
2016-08-05 18:00:53 -0700

    [360. Sort Transformed Array][Accepted]committed by Di

--- 360-Sort-Transformed-Array/solution.java
+++ 360-Sort-Transformed-Array/solution.java
@@ -1,15 +1,38 @@
 public class Solution {
     
-    
     public int[] sortTransformedArray(int[] nums, int a, int b, int c) {
         if (nums == null) {
             return null;
         }
         int[] res = new int[nums.length];
         for (int i=0; i<nums.length; i++) {
-            res[i] = getTransform(a, b, c, nums[i]);
+            nums[i] = getTransform(a, b, c, nums[i]);
         }
-        Arrays.sort(res);
+        // Arrays.sort(res); // This takes O(nlogn)   
+        int index = a>0?nums.length-1:0;
+        int i=0;
+        int j=nums.length-1;
+        while (i <= j) {
+            if (a>0) {
+                if (nums[i]>nums[j]) {
+                    res[index] = nums[i];
+                    i++;
+                } else {
+                    res[index] = nums[j];
+                    j--;
+                }
+                index--;
+            } else {
+                if (nums[i]<nums[j]) {
+                    res[index] = nums[i];
+                    i++;
+                } else {
+                    res[index] = nums[j];
+                    j--;
+                }
+                index++;
+            }
+        }
         return res;
     }
     
