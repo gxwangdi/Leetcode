21b58b051b8026843ecdb74dbc4c29effb37216d
Di Wang <gxwangdi@users.noreply.github.com>
2016-08-06 18:50:53 -0700

    [44. Wildcard Matching][Accepted]committed by Di

--- 44-Wildcard-Matching/solution.java
+++ 44-Wildcard-Matching/solution.java
@@ -1,5 +1,41 @@
 public class Solution {
     
+    // Two pointer approach
+    public boolean isMatch(String s, String p) {
+        int i = 0;
+        int j = 0;
+        int match = 0;
+        int starIdx = -1;
+        while (i < s.length()) {
+            // advancing both pointers
+            if ( j < p.length() && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '?') ) {
+                i++;
+                j++;
+            }
+            // * found, only advancing pattern pointer
+            else if (j<p.length() && p.charAt(j)=='*') {
+                starIdx = j;
+                match = i;
+                j++;
+            }
+            // last pattern pointer was '*', advancing string pointer
+            else if (starIdx != -1) { 
+                // Note how this deals with pop stack and go to another flow. 
+                j = starIdx + 1;
+                match++;
+                i = match;
+            }
+            else {
+                return false;
+            }
+        }
+        while (j<p.length() && p.charAt(j) == '*') {
+            j++;
+        }
+        return j == p.length();
+    }
+    
+    /*
     // DP approach
     public boolean isMatch(String s, String p) {
         int m = s.length();
@@ -22,12 +58,17 @@ public class Solution {
                     dp[i][j] = dp[i-1][j-1] 
                         && (s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '?');
                 } else {
-                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                    // dp[i-1][j] means I take the '*' at pos j-1 as empty 
+                    // dp[i][j-1] means I take the '*' at pos j-1 to match the char c at pos i-1. 
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1]; 
                 }
             }
         }
         return dp[m][n];          
     }
+    */
+    
+    
     
     /*
     // Recursive manner,  TLE
