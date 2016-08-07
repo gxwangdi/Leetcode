ec52d7cca34e68b74bafa4121a2a3169751746ce
Di Wang <gxwangdi@users.noreply.github.com>
2016-08-07 16:39:29 -0700

    [212. Word Search II][Accepted]committed by Di

---
+++ 212-Word-Search-II/solution.java
@@ -0,0 +1,140 @@
+public class Solution {
+    
+    public List<String> findWords(char[][] board, String[] words) {
+        List<String> res = new ArrayList<>();
+        TrieNode root = buildTrie(words);
+        for (int i=0; i<board.length; i++) {
+            for (int j=0; j<board[0].length; j++) {
+                dfs(res, board, i, j, root);
+            }
+        }
+        return res;
+    }// end of findWords
+    
+    private void dfs(List<String> res, char[][] board, int i, int j, TrieNode p) {
+        char c = board[i][j];
+        if (c == '#' || p.next[c-'a'] == null) {
+            return;
+        }
+        p = p.next[c-'a'];
+        if (p.word!=null) {
+            res.add(p.word);
+            p.word = null;
+        }
+        board[i][j] = '#';
+        if (i>0) {
+            dfs(res, board, i-1, j, p);
+        } 
+        if (j>0) {
+            dfs(res, board, i, j-1, p);
+        }
+        if (i<board.length-1) {
+            dfs(res, board, i+1, j, p);
+        }
+        if (j<board[0].length-1) {
+            dfs(res, board, i, j+1, p);
+        }
+        board[i][j] = c;
+    }
+    
+    private TrieNode buildTrie(String[] words) {
+        TrieNode root = new TrieNode();
+        for (String s:words) {
+            TrieNode p = root;
+            char[] arr = s.toCharArray();
+            for (char c: arr) {
+                int index = c-'a';
+                if (p.next[index] == null) {
+                    p.next[index] = new TrieNode();
+                }
+                p = p.next[index];
+            }
+            p.word = s;
+        }
+        return root;
+    }
+    
+    private static class TrieNode {
+        TrieNode[] next = new TrieNode[26];
+        String word;
+    }
+    
+    
+    /*         
+    // Using pair(x,y) in a set does not help avoid duplicates
+    // now it does not pass case like {["aa"], ["aaa"]}. 
+    public List<String> findWords(char[][] board, String[] words) {
+        List<String> res = new ArrayList<>();
+        if (board == null || board.length == 0 || words == null || words.length == 0) {
+            return res;
+        }
+        Set<String> set = new HashSet<>();
+        for (String word : words) {
+            if (isMatch(board, word)) {
+                set.add(word);
+            }
+        }
+        res.addAll(set);
+        return res;
+    }// end of findWords
+    
+    private boolean isMatch(char[][] board, String word) {
+        int m = board.length;
+        int n = board[0].length;
+        char c = word.charAt(0);
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                if (board[i][j]!=c) {
+                    continue;
+                }
+                // int[] pair = new int[2];
+                // pair[0] = i;
+                // pair[1] = j;
+                Set<Pair> s = new HashSet<>();
+                s.add(new Pair(i, j));
+                if (isMatch(board, s, i, j, word, 1)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }// end of isMatch    
+    
+    private boolean isMatch(char[][] board, Set<Pair> s, int x, int y, String word, int cur) {
+        if (cur == word.length()) {
+            return true;
+        }
+        for (int i=x-1; i<=x+1; i++) {
+            for (int j=y-1; j<=y+1; j++) {
+                if (i<0) continue;
+                if (i>=board.length) continue;
+                if (j<0) continue;
+                if (j>=board[0].length) continue;
+                // int[] pair = {i, j};
+                Pair p = new Pair(i, j);
+                if (s.contains(p)) continue;
+                if (word.charAt(cur)!=board[i][j]) continue;
+                s.add(p);
+                if (isMatch(board, s, i, j, word, cur+1)) {
+                    return true;
+                }
+                s.remove(p);
+            }
+        }
+        return false;
+    }
+    
+    private static class Pair {
+        public int x;
+        public int y;
+        public Pair(int i, int j) {
+            x = i;
+            y = j;
+        }
+    }// end of Pair     
+    */
+}
+
+
+
+
