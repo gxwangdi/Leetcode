127d3fd5663048f1263b23457c7bf2664b2e4fcd
Di Wang <gxwangdi@users.noreply.github.com>
2016-08-08 11:34:38 -0700

    [218. The Skyline Problem][Accepted]committed by Di

---
+++ 218-The-Skyline-Problem/solution.java
@@ -0,0 +1,40 @@
+public class Solution {
+    public List<int[]> getSkyline(int[][] buildings) {
+        List<int[]> res = new ArrayList<>();          
+        List<int[]> height = new ArrayList<>();       
+        
+        for (int[] arr:buildings) {
+            height.add(new int[]{arr[0], -arr[2]});
+            height.add(new int[]{arr[1], arr[2]});
+        }
+        
+        Collections.sort(height, new Comparator<int[]>(){
+            public int compare(int[] a, int[] b) {
+                if (a[0] != b[0]) {
+                    return a[0]-b[0];
+                }
+                return a[1]-b[1];
+            }
+        });
+        
+        Queue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        maxHeap.offer(0);
+        int prev = 0;
+        for (int[] h:height) {
+            if (h[1]<0) {
+                maxHeap.offer(-h[1]);
+            } else {
+                maxHeap.remove(h[1]);
+            }
+            int cur = maxHeap.peek();
+            if (prev != cur) {
+                res.add(new int[]{h[0], cur});
+                prev = cur;
+            }
+        }
+        return res;
+    }// end of getSkyline
+}
+
+
+
