1749bc94eac4ae506c81d47880db9d0aeeb7c46d
Di Wang <gxwangdi@users.noreply.github.com>
2016-08-09 16:00:32 -0700

    [275. H-Index II][Accepted]committed by Di

--- 275-H-Index-II/solution.java
+++ 275-H-Index-II/solution.java
@@ -1,4 +1,63 @@
 public class Solution {
+    
+    // Binary Search
+    public int hIndex(int[] citations) {
+        if (citations == null || citations.length == 0) {
+            return 0;
+        }
+        int len = citations.length;
+        int left = 0;
+        int right = len-1;
+        int mid;
+        while (left <= right) {
+            mid=left+ (right-left)/2;
+            if(citations[mid] >= (len-mid)) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return len - left;
+    }
+    
+    /*
+    // Binary search
+    public int hIndex(int[] citations) {
+        if (citations == null || citations.length == 0) {
+            return 0;
+        }
+        int n = citations.length;
+        // if (citations[0]>=n) {
+        //     return n;
+        // }
+        int start = 0;
+        int end = n-1;
+        while (start + 1 < end) {
+            int mid = start + (end-start)/2;
+            int h = n - mid;
+            if (citations[mid] == h) {
+                start = mid;
+            } else if (citations[mid] < h) {
+                start = mid +1;
+            } else {
+                end = mid;
+            }
+        }
+        
+        int h = n - end;
+        if (citations[end]>=h) {
+            return h;
+        }
+        h = n - start;
+        if (citations[start]>=h) {
+            return h;
+        }
+        return citations.length;
+    }
+    */
+    
+    /*
+    // O(n)
     public int hIndex(int[] citations) {
         int res = 0;
         if (citations == null || citations.length == 0) {
@@ -22,6 +81,7 @@ public class Solution {
         }
         return res;
     }
+    */
 }
 
 
