c1271189f4e815b78c29b3e60628c4a1f7a8685c
Di Wang <gxwangdi@users.noreply.github.com>
2016-08-08 21:23:48 -0700

    [10. Regular Expression Matching][Accepted]committed by Di

--- 10-Regular-Expression-Matching/solution.java
+++ 10-Regular-Expression-Matching/solution.java
@@ -1,13 +1,40 @@
 public class Solution {
     
+    /* // Java API is accepted. 
     public boolean isMatch(String s, String p) {
         if (s == null || p == null) {
             return false;
         }
         return s.matches(p);
+    }*/ 
+    
+    public boolean isMatch(String s, String p) {
+        if (p.isEmpty()) {
+            return s.isEmpty();
+        }
+        
+        if (p.length()==1 || p.charAt(1)!='*') {
+            if (s.isEmpty() || (p.charAt(0)!='.' && p.charAt(0)!=s.charAt(0)) ) {
+                return false;
+            } else {
+                return isMatch(s.substring(1), p.substring(1));
+            }
+        }
+        
+        while ( !s.isEmpty() && (s.charAt(0)==p.charAt(0) || p.charAt(0)=='.') ) {
+            if ( isMatch(s, p.substring(2)) ) {
+                return true;
+            }
+            s = s.substring(1);
+        }
+        
+        return isMatch( s, p.substring(2) );
     }
     
-    /* //
+    
+    
+    
+    /* // recursive two pointers approach, does not work for now.
     public boolean isMatch(String s, String p) {
         if (s == null || p == null) {
             return false;
