2b37a7642351871d5d2ee54acad6f50182f2aa7e
Di Wang <gxwangdi@users.noreply.github.com>
2016-08-12 00:59:42 -0700

    [373. Find K Pairs with Smallest Sums][Accepted]committed by Di

--- 373-Find-K-Pairs-with-Smallest-Sums/solution.java
+++ 373-Find-K-Pairs-with-Smallest-Sums/solution.java
@@ -1,5 +1,36 @@
 public class Solution {
     
+    // This shows a way to keep track of the index in an easy/straightforward way. 
+    public List<int[]> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        List<int[]> ret = new ArrayList<int[]>();
+        if (nums1.length == 0 || nums2.length == 0 || k == 0) {
+            return ret;
+        }
+        
+        int[] index = new int[nums1.length];
+        while (k-- > 0) {
+            int min_val = Integer.MAX_VALUE;
+            int in = -1;
+            for (int i = 0; i < nums1.length; i++) {
+                if (index[i] >= nums2.length) {
+                    continue;
+                }
+                if (nums1[i] + nums2[index[i]] < min_val) {
+                    min_val = nums1[i] + nums2[index[i]];
+                    in = i;
+                }
+            }
+            if (in == -1) {
+                break;
+            }
+            int[] temp = {nums1[in], nums2[index[in]]};
+            ret.add(temp);
+            index[in]++;
+        }
+        return ret;
+    }
+    
+    /*
     // Priority queue approach.  Time O(klogk)   Space O(m).  
     public List<int[]> kSmallestPairs(int[] nums1, int[] nums2, int k) {
         List<int[]> res = new ArrayList<>(k);
@@ -20,6 +51,11 @@ public class Solution {
         }
         return res;
     }
+    */
+    
+    
+    
+    
     
     /*
     // My implementation does not handle duplicate elements for now. 
