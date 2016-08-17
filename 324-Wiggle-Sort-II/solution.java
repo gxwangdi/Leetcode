4e87f9039b0abd6ad38cd4d4109ae7e938b2f7f6
Di Wang <gxwangdi@users.noreply.github.com>
2016-08-16 21:46:23 -0700

    [324. Wiggle Sort II][Accepted]committed by Di

--- 324-Wiggle-Sort-II/solution.java
+++ 324-Wiggle-Sort-II/solution.java
@@ -1,5 +1,37 @@
 public class Solution {
     
+    // Space O(n), easier to understand version.   
+    public void wiggleSort(int[] nums) {
+    int median=findKthLargest(nums,(nums.length+1)/2);
+    int odd=1;
+    int even=nums.length%2==0?nums.length-2:nums.length-1;
+    int[] tmpArr=new int[nums.length];
+    for(int i=0;i<nums.length;i++){
+        if(nums[i]>median){
+            tmpArr[odd]=nums[i];
+            odd+=2;
+            continue;
+        }
+        if(nums[i]<median){
+            tmpArr[even]=nums[i];
+            even-=2;
+            continue;
+        }
+    }
+    while(odd<nums.length){
+        tmpArr[odd]=median;
+        odd+=2;
+    }
+    while(even>=0){
+        tmpArr[even]=median;
+        even-=2;
+    }
+    for(int i=0;i<nums.length;i++){
+        nums[i]=tmpArr[i];
+    }
+}
+    
+    /*
     // Index Mapping.  
     public void wiggleSort(int[] nums) {
         int median = findKthLargest(nums, (nums.length + 1) / 2);
@@ -29,6 +61,7 @@ public class Solution {
         arr[i] = arr[j];
         arr[j] = temp;
     }
+    */
     
     
     public int findKthLargest(int[] nums, int k) {
