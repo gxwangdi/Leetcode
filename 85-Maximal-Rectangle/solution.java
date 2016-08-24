fd95babbe325972b2f57b97d97c70b492f7f7e59
Di Wang <gxwangdi@users.noreply.github.com>
2016-08-24 14:59:47 -0700

    [85. Maximal Rectangle][Accepted]committed by Di

--- 85-Maximal-Rectangle/solution.java
+++ 85-Maximal-Rectangle/solution.java
@@ -1,4 +1,54 @@
 public class Solution {
+    
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) return 0;
+        int n = matrix.length, m = matrix[0].length;
+        int[] array = new int[m];
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            update(matrix, i, array);
+            max = Math.max(max, findMax(array));
+        }
+        return max;
+    }
+    
+    public void update(char[][] matrix, int row, int[] array) {
+        int m = array.length;
+        for (int i = 0; i < m; i++) {
+            array[i] = matrix[row][i] == '0' ? 0 : 1 + array[i];
+        }
+    }
+    
+    public int findMax(int[] array) {
+        Deque<Integer> stack = new LinkedList<Integer>();
+        stack.push(-1);
+        int max = 0;
+        
+        for (int i = 0; i < array.length; i++) {
+            if (stack.peek() == -1 || array[i] >= array[stack.peek()]) {
+                stack.push(i);
+            } else {
+                while (stack.peek() != -1 && array[stack.peek()] > array[i]) {
+                    int h = array[stack.pop()];
+                    int l = stack.peek();
+                    int r = i;
+                    max = Math.max(max, h * (r - l - 1));
+                }
+                stack.push(i);
+            }
+        }
+        
+        while (stack.peek() != -1) {
+            int h = array[stack.pop()];
+            int l = stack.peek();
+            int r = array.length;
+            max = Math.max(max, h * (r - l - 1));
+        }
+        
+        return max;
+    }
+    
+    /*
     public int maximalRectangle(char[][] matrix) {
         if (matrix == null) {
             return 0;
@@ -8,6 +58,7 @@ public class Solution {
             return 0;
         }
         int n = matrix[0].length;
+        //bool dp[m][n][m+1][n+1];
         boolean[][] dp = new boolean[2][n+1];
         int maxRectangle = 0;
         for (int i=0; i<m; i++) {
@@ -22,8 +73,10 @@ public class Solution {
                             break;
                         }
                         if (xLen == 1 && yLen == 1) {
+                            //dp[i][j][xLen][yLen] = true;
                             dp[1][yLen] = true;
                         } else if (xLen == 1 && yLen != 1) {
+                            //dp[i][j][xLen][yLen] = dp[i][j][xLen][yLen-1];
                             dp[1][yLen] = dp[1][yLen-1];
                         } else if (xLen != 1 && yLen == 1) {
                             dp[1][yLen] = dp[0][yLen];
@@ -41,7 +94,8 @@ public class Solution {
             }
         }
         return maxRectangle;
-    }// end of maximalRectangle     
+    }// end of maximalRectangle        
+    */
 }
 
 
