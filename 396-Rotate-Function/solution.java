594521560906164e03c901b20aeeb9a1f01263c2
Di Wang <gxwangdi@users.noreply.github.com>
2016-09-15 15:29:36 -0700

    [396. Rotate Function][Accepted]committed by Di

---
+++ 396-Rotate-Function/solution.java
@@ -0,0 +1,35 @@
+public class Solution {
+    
+    public int maxRotateFunction(int[] A) {
+        int F = 0;
+        int sum = 0;
+        int res = Integer.MIN_VALUE;   
+        int n = A.length;
+        if ( n<=1 ) {
+            return 0;
+        }
+        for (int i=0; i<n; i++) {
+            F += i*A[i];
+            sum += A[i];
+        }
+        
+        if (F > res) {
+            res = F;
+        }
+        for (int k=1; k<n; k++) {
+            F += sum;
+            F -= n*A[n-k];
+            if ( F>res ) {
+                res = F;
+            }
+        }
+        return res;        
+    }// end of maxRotateFunction      
+    
+}
+
+
+
+
+
+
