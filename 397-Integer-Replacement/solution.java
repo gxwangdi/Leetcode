044720bb9dffc8aa9fa85ce56be9282e854a7754
Di Wang <gxwangdi@users.noreply.github.com>
2016-09-15 14:39:33 -0700

    [397. Integer Replacement][Accepted]committed by Di

---
+++ 397-Integer-Replacement/solution.java
@@ -0,0 +1,41 @@
+public class Solution {
+    public int integerReplacement(int n) {
+        long N = n;
+        long small, big;
+        int count = 0;
+        while (N!=1) {
+            small = (N & (N-1));
+            big = (N & (N+1));
+            if ( (N&1)==0 ) {
+                N >>=1;
+            } else if ( (small & (small-1)) <= (big & (big-1)) ) {
+                N = N-1;
+            } else {
+                N = N+1;
+            }
+            count++;
+        }
+        return count;
+    }
+    /*
+    public int integerReplacement(int n) {
+        if (n <= 1) {
+            return 0;
+        }
+        int count = 0;
+        int cur = 1;
+        while (cur < n) {
+            cur = cur<<1;
+            count++;
+        }
+        if (cur == n) {
+            return count;
+        }
+        return count+1;
+    }// end of integerReplacement  
+    */
+}
+
+
+
+
