9b01fa984d0f404f346a6e4f1625f62bde223082
Di Wang <gxwangdi@gmail.com>
2019-06-11 21:59:37 -0700

    Added solution for Palindrome Number in cpp, go, js, python.

--- 9-Palindrome-Number/PalindromeNumber.java
+++ 9-Palindrome-Number/PalindromeNumber.java
@@ -1,10 +1,7 @@
 public class PalindromeNumber {
     public boolean isPalindrome(int x) {
-        if (x<0)
-            return false;
-        
         String s = Integer.toString(x);
-        int i =0; 
+        int i =0;
         int j =s.length()-1;
         while (i<j) {
             if (s.charAt(i) != s.charAt(j)) {
