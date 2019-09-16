9f7f0b9153f35799a212b7e890295549a4581413
Di Wang <gxwangdi@gmail.com>
2019-09-15 20:57:02 -0700

    Added solution for rectangle area in cpp, go, js and python.

--- 223-Rectangle-Area/RectangleArea.java
+++ 223-Rectangle-Area/RectangleArea.java
@@ -3,7 +3,7 @@ public class RectangleArea {
         if (A>G || E>C) {
             return (C-A)*(D-B) + (G-E)*(H-F);
         }
-        
+
         if (F>D || B>H) {
             return (C-A)*(D-B) + (G-E)*(H-F);
         }
@@ -12,7 +12,7 @@ public class RectangleArea {
         int left = Math.max(A,E);
         int top = Math.min(H,D);
         int bottom = Math.max(F,B);
- 
-        return (G-E)*(H-F) + (C-A)*(D-B) - (right-left)*(top-bottom);
+
+        return (G-E)*(H-F) + ((C-A)*(D-B) - (right-left)*(top-bottom));
     }
 }
