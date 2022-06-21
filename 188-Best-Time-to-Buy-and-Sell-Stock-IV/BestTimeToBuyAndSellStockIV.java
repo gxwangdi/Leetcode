bb498ac3aa2d53fc4d7fc548827914225854f3a6
gxwangdi <gxwangdi@gmail.com>
2022-06-21 10:52:33 -0700

    188 java solution
    188 java solution

--- 188-Best-Time-to-Buy-and-Sell-Stock-IV/BestTimeToBuyAndSellStockIV.java
+++ 188-Best-Time-to-Buy-and-Sell-Stock-IV/BestTimeToBuyAndSellStockIV.java
@@ -1,33 +1,63 @@
 public class BestTimeToBuyAndSellStockIV {
-    
+
+  public int maxProfit(int k, int[] prices) {
+       if (k == 0) return 0;
+
+       int[] profit = new int[k+1];
+       int[] cost = new int[k+1];
+
+       profit[0] = 0;
+       Arrays.fill(cost, Integer.MAX_VALUE);
+
+       for (int price: prices) {
+           for (int i = 0; i < k; i++) {
+               cost[i+1] = Math.min(cost[i+1], price - profit[i]);
+               profit[i+1] = Math.max(profit[i+1], price - cost[i+1]);
+           }
+       }
+       return profit[k];
+   }
+
+
+  // Sell and buy stock II
+  // private int solveMaxProfit(int[] prices) {
+  //     int max = 0;
+  //     for (int i=1; i<prices.length; i++) {
+  //         if (prices[i] > prices[i-1]) {
+  //             max += prices[i] - prices[i-1];
+  //         }
+  //     }
+  //     return max;
+  // }// end of solveMaxProfit
+
     // DP: t(i,j) is the max profit for up to i transactions by time j (0<=i<=K, 0<=j<=T).
-    public int maxProfit(int k, int[] prices) {
-        int len = prices.length;
-        if (k >= len / 2) return solveMaxProfit(prices);
-        
-        int[][] t = new int[k + 1][len];
-        for (int i = 1; i <= k; i++) {
-            int tmpMax =  -prices[0];
-            for (int j = 1; j < len; j++) {
-                t[i][j] = Math.max(t[i][j - 1], prices[j] + tmpMax); 
-                // 有i次交易 到第j-1天时候的最大值， 以及第j天时候的价格和累积最小成本的最大值 max取后者表明我要在第j天进行交易
-                tmpMax =  Math.max(tmpMax, t[i - 1][j - 1] - prices[j]);  
-                // 少一次交易少一天时候的最大值＋当前最小成本， max取后者表明交易算上那一天  因为那是最小成本
-            }
-        }
-        return t[k][len - 1];
-    }
-    
+    // public int maxProfit(int k, int[] prices) {
+    //     int len = prices.length;
+    //     if (k >= len / 2) return solveMaxProfit(prices);
+    //
+    //     int[][] t = new int[k + 1][len];
+    //     for (int i = 1; i <= k; i++) {
+    //         int tmpMax =  -prices[0];
+    //         for (int j = 1; j < len; j++) {
+    //             t[i][j] = Math.max(t[i][j - 1], prices[j] + tmpMax);
+    //             // 有i次交易 到第j-1天时候的最大值， 以及第j天时候的价格和累积最小成本的最大值 max取后者表明我要在第j天进行交易
+    //             tmpMax =  Math.max(tmpMax, t[i - 1][j - 1] - prices[j]);
+    //             // 少一次交易少一天时候的最大值＋当前最小成本， max取后者表明交易算上那一天  因为那是最小成本
+    //         }
+    //     }
+    //     return t[k][len - 1];
+    // }
+
     /*
     public int maxProfit(int k, int[] prices) {
         if (k<=0 || prices == null || prices.length == 0) {
             return 0;
         }
-        
-        if (k>=prices.length/2) {  // One transaction is buy and sell.    
-            return solveMaxProfit(prices); 
+
+        if (k>=prices.length/2) {  // One transaction is buy and sell.
+            return solveMaxProfit(prices);
         }
-        
+
         int[] g = new int[k+1];
         int[] l = new int[k+1];
         for (int i=0; i<prices.length-1; i++) {
@@ -40,18 +70,4 @@ public class BestTimeToBuyAndSellStockIV {
         return g[k];
     }
     */
-    
-    // Sell and buy stock II
-    private int solveMaxProfit(int[] prices) {
-        int max = 0;
-        for (int i=1; i<prices.length; i++) {
-            if (prices[i] > prices[i-1]) {
-                max += prices[i] - prices[i-1];
-            }
-        }
-        return max;
-    }// end of solveMaxProfit     
 }
-
-
-
