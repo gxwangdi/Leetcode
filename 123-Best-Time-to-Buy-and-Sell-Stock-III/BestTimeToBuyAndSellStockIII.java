a37b09f459582bae83778d38da919a6388323239
gxwangdi <gxwangdi@gmail.com>
2022-06-21 10:38:10 -0700

    123 java solution
    123 java solution

--- 123-Best-Time-to-Buy-and-Sell-Stock-III/BestTimeToBuyAndSellStockIII.java
+++ 123-Best-Time-to-Buy-and-Sell-Stock-III/BestTimeToBuyAndSellStockIII.java
@@ -1,41 +1,57 @@
 public class BestTimeToBuyAndSellStockIII {
-    
+
     // l[i] means the max profit for [0...i]  r[i] means the max profit for [i...n]
+    // Time O(n) 3 rounds   Space O(n)
+    // public int maxProfit(int[] prices) {
+    //     if (prices == null || prices.length == 0) {
+    //         return 0;
+    //     }
+    //
+    //     int max = 0;
+    //     int[] left = new int[prices.length];
+    //     int[] right = new int[prices.length];
+    //
+    //     process(prices, left, right);
+    //     for (int i=0; i<prices.length; i++) {
+    //         max = Math.max(max, left[i] + right[i]);
+    //     }
+    //     return max;
+    // }
+    //
+    //
+    // private void process(int[] prices, int[] left, int[] right) {
+    //     left[0] = 0;
+    //     int min = prices[0];
+    //
+    //     for (int i=1; i<left.length; i++) {
+    //         left[i] = Math.max(left[i-1], prices[i]-min);
+    //         min = Math.min(min, prices[i]);
+    //     }
+    //
+    //     right[right.length-1] = 0;
+    //     int max = prices[right.length-1];
+    //     for (int i=right.length-2; i>=0; i--) {
+    //         right[i] = Math.max(right[i+1], max - prices[i]);
+    //         max = Math.max(max, prices[i]);
+    //     }
+    // }
+
+    // Time O(n) Space O(1)
     public int maxProfit(int[] prices) {
-        if (prices == null || prices.length == 0) {
-            return 0;
+        int buyOne = Integer.MAX_VALUE;
+        int SellOne = 0;
+        int buyTwo = Integer.MAX_VALUE;
+        int SellTwo = 0;
+        for(int p : prices) {
+            buyOne = Math.min(buyOne, p);
+            SellOne = Math.max(SellOne, p - buyOne);
+            buyTwo = Math.min(buyTwo, p - SellOne);
+            SellTwo = Math.max(SellTwo, p - buyTwo);
         }
-        
-        int max = 0;
-        int[] left = new int[prices.length];
-        int[] right = new int[prices.length];
-        
-        process(prices, left, right);
-        for (int i=0; i<prices.length; i++) {
-            max = Math.max(max, left[i] + right[i]);
-        }
-        return max;
+        return SellTwo;
     }
-    
-    
-    private void process(int[] prices, int[] left, int[] right) {
-        left[0] = 0;
-        int min = prices[0];
-        
-        for (int i=1; i<left.length; i++) {
-            left[i] = Math.max(left[i-1], prices[i]-min);
-            min = Math.min(min, prices[i]);
-        }
-        
-        right[right.length-1] = 0;
-        int max = prices[right.length-1];
-        for (int i=right.length-2; i>=0; i--) {
-            right[i] = Math.max(right[i+1], max - prices[i]);
-            max = Math.max(max, prices[i]);
-        }
-    }
-    
-    
+
+
     /*
     // Naive approach, accepted on lintcode, TLE on leetcode.
     public int maxProfit(int[] prices) {
@@ -43,7 +59,7 @@ public class BestTimeToBuyAndSellStockIII {
         if (prices == null || prices.length == 0) {
             return maxProfit;
         }
-        
+
         int[] dp = new int[prices.length];
         int profit;
         for (int i=0; i<prices.length; i++) {
@@ -51,13 +67,13 @@ public class BestTimeToBuyAndSellStockIII {
             profit+= maxProfit(prices, i+1, prices.length-1);
             dp[i] = profit;
         }
-        
+
         for (int i:dp) {
             maxProfit = Math.max(maxProfit, i);
         }
         return maxProfit;
     }
-    
+
     private int maxProfit(int[] src, int start, int end) {
         if (start >= end) {
             return 0;
@@ -73,7 +89,3 @@ public class BestTimeToBuyAndSellStockIII {
     }
     */
 }
-
-
-
-
